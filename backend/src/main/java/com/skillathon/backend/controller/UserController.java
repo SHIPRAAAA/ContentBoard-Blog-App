8e58ca8e81bf8655313378640bf7f17cea18e6b3
Suvadeep <guhasukumarrahul0@gmail.com>
2023-02-25 01:51:53 +0530

    Spring Backend

---
+++ backend/src/main/java/com/skillathon/backend/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.skillathon.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillathon.backend.model.UserModel;
+import com.skillathon.backend.service.UserService;
+
+@Component
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	/*@GetMapping
+	public String Getalluser() {
+		return "I am working ";
+	}*/
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<UserModel> getAllUser(){
+		return userService.getAllUser();
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public UserModel getUserById(@PathVariable long id) {
+		return userService.getUserById(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.OK)
+	public UserModel addUser(@RequestBody UserModel user) {
+		return userService.addUser(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteUserById(@PathVariable long id) {
+		userService.deleteUser(id);
+	}
+	
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	public UserModel updateUser(@RequestBody UserModel user) {
+		return userService.updateUser(user);
+	}
+}
+
+
---
+++ backend/src/main/java/com/skillathon/backend/service/UserService.java
@@ -0,0 +1,46 @@
+package com.skillathon.backend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.skillathon.backend.model.UserModel;
+import com.skillathon.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+	
+	@Autowired
+	private final UserRepository userRepository ;
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public List<UserModel> getAllUser(){
+		System.out.println(userRepository.findAll().toString());
+		return userRepository.findAll();
+		
+	}
+
+	
+	public UserModel getUserById(long id) {
+		return userRepository.findById(id).orElseThrow(()->new RuntimeException("Not Found" + id));
+	}
+	
+	public UserModel addUser(UserModel user) {
+		return userRepository.save(user);
+	}
+	
+	public void deleteUser(long id) {
+		UserModel user = getUserById(id);
+		userRepository.delete(user);
+	}
+	
+	public UserModel updateUser(UserModel user) {
+		return userRepository.save(user);
+	}
+}
+
+
