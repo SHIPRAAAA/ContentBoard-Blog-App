e2cca0ef66fe26476f1c336eab0722bdd4b4b2aa
Souvik Ghosh <xandersavy@gmail.com>
2023-02-25 21:26:21 +0530

    UserModel half done

--- backend/src/main/java/com/skillathon/backend/controller/JobController.java
+++ backend/src/main/java/com/skillathon/backend/controller/JobController.java
@@ -18,44 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skillathon.backend.model.JobModel;
 import com.skillathon.backend.service.JobService;
 
-
 @Component
 @RestController
 @RequestMapping("/job")
 public class JobController {
 
-	/*@GetMapping
-	public String Getalluser() {
-		return "I am working ";
-	}*/
-	
+	/*
+	 * @GetMapping
+	 * public String Getalluser() {
+	 * return "I am working ";
+	 * }
+	 */
+
 	@Autowired
 	private JobService jobService;
 
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	public List<JobModel> getAllJob(){
+	public List<JobModel> getAllJob() {
 		return jobService.getAllJob();
 	}
-	
+
 	@GetMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public JobModel getJobById(@PathVariable long id) {
 		return jobService.getJobById(id);
 	}
-	
-	@PostMapping
+
+	@PostMapping("")
 	@ResponseStatus(HttpStatus.OK)
 	public JobModel addJob(@RequestBody JobModel job) {
 		return jobService.addJob(job);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteJobById(@PathVariable long id) {
 		jobService.deleteJob(id);
 	}
-	
+
 	@PutMapping
 	@ResponseStatus(HttpStatus.OK)
 	public JobModel updateJob(@RequestBody JobModel job) {
